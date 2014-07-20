7412466532e22e8ed99de14db038d445540b8381
Peter <pliu@palantir.com>
2014-07-20 02:04:46 -0700

    asdfsda

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,8 @@
  */
 
 exports.index = function(req, res){
-	var Firebase = require('firebase');
-	var jQuery = require('jquery');
 	var testData = new Firebase("https://scorching-fire-2712.firebaseio.com/");
-	var $ = new jQuery();
-	
-	$('#user').keypress(function (e) {
-		if (e.keyCode == 13) {
-			var balls = $('#name').val();
-			testData.push({ testes: balls });
-		}
-	});
 
-	// console.log(testData);
 	testData.on("value", function(data) {
 		var message = data.val();
 		res.render('index', { title: message.testes });
