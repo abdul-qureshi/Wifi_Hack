f5f4fe07b574edb4fe551bb8f57245611c16a637
Peter <pliu@palantir.com>
2014-07-20 02:00:48 -0700

    asdfasd

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ exports.index = function(req, res){
 	var Firebase = require('firebase');
 	var jQuery = require('jquery');
 	var testData = new Firebase("https://scorching-fire-2712.firebaseio.com/");
-
+	var $ = new jQuery();
+	
 	$('#user').keypress(function (e) {
 		if (e.keyCode == 13) {
 			var balls = $('#name').val();
