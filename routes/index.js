87310d4b1e5ecf37f904b50e0a8641736e8c3a32
Peter <pliu@palantir.com>
2014-07-20 01:30:06 -0700

    asdfsad

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,6 @@
  * GET home page.
  */
 
-var Firebase = require('firebase');
-
-var testData = new Firebase("https://scorching-fire-2712.firebaseio.com/");
-testData.set({ testes: "hi"});
-
-// console.log(testData);
-testData.on("value", function(data) {
-	var message = data.val();
-	console.log(message);
-});
-
 exports.index = function(req, res){
   res.render('index', { title: 'Peter Liu' });
 };
