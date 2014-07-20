b90043d6dd2f4caca2cbed0612c56e6bb783663e
Peter <pliu@palantir.com>
2014-07-19 23:39:03 -0700

    asdf

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  var testData = new Firebase('https://scorching-fire-2712.firebaseio.com/');
+  var testData = new Firebase("https://scorching-fire-2712.firebaseio.com/");
   testData.on('value', function(dataSnapshot) {
 	var message = dataSnapshot.val();
 	console.log(message.test1);
