06b30b5fb5832d20b75d9e5e170a4c09cf19ca8c
Peter <pliu@palantir.com>
2014-07-20 02:15:45 -0700

    asdfsadf

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,6 @@
  */
 
 exports.index = function(req, res){
-	var Firebase = require('firebase');
-	var testData = new Firebase("https://scorching-fire-2712.firebaseio.com/");
-
-	// console.log(testData);
-	testData.on("value", function(data) {
-		var message = data.val();
-		res.render('index', { title: message.testes });
-	});
+	res.render('index', { title: "" });
 };
 
