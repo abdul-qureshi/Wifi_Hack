f741e6782fbe6b4e542b7602c7e26eecf12d2a52
Peter <pliu@palantir.com>
2014-07-19 23:31:36 -0700

    karaoke

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@
  */
 
 exports.index = function(req, res){
+  var testData = new Firebase('https://scorching-fire-2712.firebaseio.com/');
+  testData.on('value', function(dataSnapshot) {
+	var message = dataSnapshot.val();
+	console.log(message.test1);
+  });
   res.render('index', { title: 'Peter Liu' });
 };
+
